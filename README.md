7d7d96380698073399b697bdc07e18dbe8350d4d
zhangxishuo <zhangxishuo1998@outlook.com>
2019-12-29 20:35:27 +0800

    remove

--- README.md
+++
@@ -1,1 +0,0 @@
-# yunzhi-spring-boot-starter
--- src/main/resources/db/migration/V1.0.0__INIT_TABLES.sql
+++
@@ -1,8 +0,0 @@
--- 创建用户表
-CREATE TABLE user (
-  id bigint(20) NOT NULL AUTO_INCREMENT COMMENT '用户id',
-  username varchar(255) NOT NULL DEFAULT '' COMMENT '用户名',
-  password varchar(255) NOT NULL DEFAULT '' COMMENT '密码',
-  PRIMARY KEY (id),
-  UNIQUE KEY (username)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
