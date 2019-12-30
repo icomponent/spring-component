df6239e0503d624d9571db63abee111868ba4f3f
zhangxishuo <zhangxishuo1998@outlook.com>
2019-12-30 21:26:33 +0800

    nullable-and-notnull

---
+++ spring-core/src/main/java/club/icomponent/lang/NotNull.java
@@ -0,0 +1,13 @@
+package club.icomponent.lang;
+
+import javax.annotation.Nonnull;
+import javax.annotation.meta.TypeQualifierNickname;
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD, ElementType.PARAMETER, ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Nonnull
+@TypeQualifierNickname
+public @interface NotNull {
+}
