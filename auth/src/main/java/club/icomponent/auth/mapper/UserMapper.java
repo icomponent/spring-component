92f6536a8ab061edc05ab1ec62c420201675785e
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-21 23:46:02 +0800

    user-mapper

--- auth/src/main/java/club/icomponent/auth/mapper/UserMapper.java
+++ auth/src/main/java/club/icomponent/auth/mapper/UserMapper.java
@@ -1,4 +1,10 @@
 package club.icomponent.auth.mapper;
 
+import club.icomponent.common.auth.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface UserMapper {
+
+    User selectUserByUsername(String username);
 }
--- auth/src/test/java/club/icomponent/auth/mapper/UserMapperTest.java
+++ auth/src/test/java/club/icomponent/auth/mapper/UserMapperTest.java
@@ -1,5 +1,24 @@
-import static org.junit.jupiter.api.Assertions.*;
+package club.icomponent.auth.mapper;
 
+import club.icomponent.common.auth.User;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@MybatisTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserMapperTest {
 
+    @Autowired
+    private UserMapper userMapper;
+
+    @Test
+    void selectUserByUsername() {
+        User user = userMapper.selectUserByUsername("admin");
+        System.out.println(user);
+    }
 }
