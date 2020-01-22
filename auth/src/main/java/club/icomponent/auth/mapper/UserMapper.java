fba79ef96f137b6d5c6d7ebd25a95dd98aa4cd85
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-22 12:37:12 +0800

    user-mapper

--- auth/src/main/java/club/icomponent/auth/mapper/UserMapper.java
+++ auth/src/main/java/club/icomponent/auth/mapper/UserMapper.java
@@ -1,10 +1,18 @@
 package club.icomponent.auth.mapper;
 
 import club.icomponent.common.auth.User;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
+    @Insert("INSERT INTO user(name, username, password) VALUES (#{name}, #{username}, #{password})")
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    void insertUser(User user);
+
+    @Select("SELECT * FROM user WHERE username=#{username}")
     User selectUserByUsername(String username);
 }
--- auth/src/test/java/club/icomponent/auth/mapper/UserMapperTest.java
+++ auth/src/test/java/club/icomponent/auth/mapper/UserMapperTest.java
@@ -1,24 +1,58 @@
 package club.icomponent.auth.mapper;
 
 import club.icomponent.common.auth.User;
+import club.icomponent.util.RandomUtils;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mybatis.spring.boot.test.autoconfigure.MybatisTest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @MybatisTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserMapperTest {
 
+    private static final Logger logger = LoggerFactory.getLogger(UserMapperTest.class);
+
     @Autowired
     private UserMapper userMapper;
 
     @Test
+    void insertUser() {
+        logger.debug("生成随机用户");
+        User user = new User();
+        user.setName(RandomUtils.randomString(10));
+        user.setUsername(RandomUtils.randomString(10));
+        user.setPassword(RandomUtils.randomString(10));
+
+        logger.debug("插入数据");
+        userMapper.insertUser(user);
+    }
+
+    @Test
     void selectUserByUsername() {
-        User user = userMapper.selectUserByUsername("admin");
-        System.out.println(user);
+        logger.debug("生成随机用户");
+        User user = new User();
+        user.setName(RandomUtils.randomString(10));
+        user.setUsername(RandomUtils.randomString(10));
+        user.setPassword(RandomUtils.randomString(10));
+
+        logger.debug("插入数据");
+        userMapper.insertUser(user);
+
+        logger.debug("查询数据");
+        User selectedUser = userMapper.selectUserByUsername(user.getUsername());
+
+        logger.debug("断言一致性");
+        assertThat(selectedUser.getId()).isNotNull();
+        assertThat(selectedUser.getName()).isEqualTo(user.getName());
+        assertThat(selectedUser.getUsername()).isEqualTo(user.getUsername());
+        assertThat(selectedUser.getPassword()).isEqualTo(user.getPassword());
     }
 }
