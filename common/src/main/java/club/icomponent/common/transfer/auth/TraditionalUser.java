8621c2cb4d72a33e87644e3a5e3ea085423e73c2
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-24 16:52:15 +0800

    traditional-user

--- common/src/main/java/club/icomponent/common/transfer/auth/TraditionalUser.java
+++ common/src/main/java/club/icomponent/common/transfer/auth/TraditionalUser.java
@@ -1,15 +1,23 @@
 package club.icomponent.common.transfer.auth;
 
+import club.icomponent.core.spec.Validation;
+import club.icomponent.core.util.StringUtils;
+
 /**
  * 传统用户登录信息
  */
-public class TraditionalUser {
+public class TraditionalUser implements Validation {
 
     private String username;
 
     private String password;
 
     public TraditionalUser() {
+    }
+
+    public TraditionalUser(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
     public String getUsername() {
@@ -26,5 +34,11 @@ public class TraditionalUser {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    @Override
+    public boolean valid() {
+        return !StringUtils.isEmpty(this.username)
+                && !StringUtils.isEmpty(this.password);
     }
 }
