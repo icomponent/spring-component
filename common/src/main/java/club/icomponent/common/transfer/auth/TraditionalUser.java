ae4abd279bae9a0ce6f13c6539f61f9927b9b24d
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-24 17:13:15 +0800

    auth-controller

--- common/src/main/java/club/icomponent/common/transfer/auth/TraditionalUser.java
+++ common/src/main/java/club/icomponent/common/transfer/auth/TraditionalUser.java
@@ -8,8 +8,14 @@ import club.icomponent.core.util.StringUtils;
  */
 public class TraditionalUser implements Validation {
 
+    /**
+     * 用户名
+     */
     private String username;
 
+    /**
+     * 密码
+     */
     private String password;
 
     public TraditionalUser() {
