b69494be8374ca98f207612cc1cb9832997123d9
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-24 17:19:59 +0800

    auth-service

--- auth/src/main/java/club/icomponent/auth/service/AuthService.java
+++ auth/src/main/java/club/icomponent/auth/service/AuthService.java
@@ -1,4 +1,8 @@
 package club.icomponent.auth.service;
 
+import club.icomponent.common.transfer.auth.TraditionalUser;
+
 public interface AuthService {
+
+    String login(TraditionalUser user);
 }
--- auth/src/main/java/club/icomponent/auth/service/api/AuthServiceImpl.java
+++ auth/src/main/java/club/icomponent/auth/service/api/AuthServiceImpl.java
@@ -1,6 +1,30 @@
 package club.icomponent.auth.service.api;
 
+import club.icomponent.auth.mapper.UserMapper;
 import club.icomponent.auth.service.AuthService;
+import club.icomponent.common.auth.User;
+import club.icomponent.common.transfer.auth.TraditionalUser;
+import org.springframework.stereotype.Service;
 
+@Service
 public class AuthServiceImpl implements AuthService {
+
+    private final UserMapper userMapper;
+
+    public AuthServiceImpl(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+
+    @Override
+    public String login(TraditionalUser traditionalUser) {
+        User user = userMapper.selectUserByUsername(traditionalUser.getUsername());
+        if (user == null) {
+            // 用户不存在，抛出 401 异常
+        }
+        if (!user.getPassword().equals(traditionalUser.getPassword())) {
+            // 密码错误，抛出 401 异常
+        }
+        // 签发 JWT 认证信息
+        return null;
+    }
 }
