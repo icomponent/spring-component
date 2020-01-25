df3e5c76801276374cfd1ea362e813b229b20a9a
zhangxishuo <zhangxishuo1998@outlook.com>
2020-01-25 22:19:48 +0800

    close-oauth

--- auth/src/main/java/club/icomponent/auth/AuthApplication.java
+++ auth/src/main/java/club/icomponent/auth/AuthApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
 @SpringBootApplication
-@EnableAuthorizationServer
+//@EnableAuthorizationServer
 public class AuthApplication {
 
     public static void main(String[] args) {
