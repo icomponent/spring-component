87ea27d48df576348947150572ddc65ae07ddd7c
zhangxishuo <zhangxishuo1998@outlook.com>
2020-02-05 20:56:24 +0800

    init-register

--- auth/src/main/java/club/icomponent/auth/controller/RegisterController.java
+++ auth/src/main/java/club/icomponent/auth/controller/RegisterController.java
@@ -1,5 +1,6 @@
 package club.icomponent.auth.controller;
 
+import club.icomponent.auth.service.UserService;
 import club.icomponent.common.transfer.auth.register.RegisterInformation;
 import club.icomponent.core.util.ValidationUtils;
 import org.slf4j.Logger;
@@ -15,9 +16,21 @@ public class RegisterController {
 
     private static final Logger logger = LoggerFactory.getLogger(RegisterController.class);
 
+    private final UserService userService;
+
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping("register")
     public void register(@RequestBody RegisterInformation information) {
         logger.debug("校验数据");
         ValidationUtils.validate(information);
+
+        logger.debug("校验验证码");
+        // TODO: 校验邮箱验证码
+
+        logger.debug("用户注册");
+        userService.register(information.getUsername(), information.getPassword());
     }
 }
