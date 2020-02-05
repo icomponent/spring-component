1c4e94cd9bd6ea1e84c6affb5963df2927c9fbb9
zhangxishuo <zhangxishuo1998@outlook.com>
2020-02-05 20:36:07 +0800

    email-validate

--- core/src/main/java/club/icomponent/core/util/StringUtils.java
+++ core/src/main/java/club/icomponent/core/util/StringUtils.java
@@ -2,6 +2,8 @@ package club.icomponent.core.util;
 
 import club.icomponent.core.lang.Nullable;
 
+import java.util.regex.Pattern;
+
 /**
  * 字符串工具类
  */
@@ -12,5 +14,17 @@ public class StringUtils {
      */
     public static boolean isEmpty(@Nullable String str) {
         return (str == null || "".equals(str));
+    }
+
+    /**
+     * 判断邮箱格式是否正确
+     */
+    public static boolean isEmail(@Nullable String email) {
+        if (isEmpty(email)) {
+            return false;
+        }
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@" +  //part before @
+                "(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        return Pattern.compile(regex).matcher(email).matches();
     }
 }
