28b89fd32bc590eea2f1f298f1de437ba48a3182
zhangxishuo <zhangxishuo1998@outlook.com>
2020-02-05 20:44:00 +0800

    test

--- common/src/test/java/club/icomponent/common/transfer/auth/login/CommonCredentialsTest.java
+++ common/src/test/java/club/icomponent/common/transfer/auth/login/CommonCredentialsTest.java
@@ -1,5 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package club.icomponent.common.transfer.auth.login;
+
+import club.icomponent.core.util.RandomUtils;
+import club.icomponent.core.util.ValidationUtils;
+import org.junit.jupiter.api.Test;
 
 class CommonCredentialsTest {
 
+    @Test
+    void valid() {
+        CommonCredentials credentials = new CommonCredentials();
+        credentials.setUsername(RandomUtils.randomString(8));
+        credentials.setPassword(RandomUtils.randomString(8));
+        ValidationUtils.validate(credentials);
+    }
 }
