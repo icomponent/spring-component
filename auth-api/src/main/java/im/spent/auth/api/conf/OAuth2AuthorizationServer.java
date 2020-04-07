329776d3f36f31afa45e23bd24369f7ac4467acc
张喜硕 <zhangxishuo1998@outlook.com>
2020-04-07 13:45:56 +0800

    auth

---
+++ auth-api/src/main/java/im/spent/auth/api/conf/OAuth2AuthorizationServer.java
@@ -0,0 +1,10 @@
+package im.spent.auth.api.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class OAuth2AuthorizationServer extends AuthorizationServerConfigurerAdapter {
+}
---
+++ auth-common/src/main/java/im/spent/auth/common/User.java
@@ -0,0 +1,12 @@
+package im.spent.auth.common;
+
+public class User {
+
+    private String id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+}
